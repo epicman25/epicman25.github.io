7a53891297f24ac5fd701b03f98248c8e7ab0e61
Devalapalli Tejeshwar Reddy <tejeshwarreddydevalapalli@gmail.com>
2022-08-14 23:25:56 +0530

    Changed the entire portfolio designn

--- README.md
+++ README.md
@@ -1,1 +1,20 @@
-# epicman25.github.io
+
+
+This is the source for my personal portfolio site of the same name.
+[Live version](https://2kabhishek.github.io)
+
+
+
+## Sections
+
+- Lead - Contains name, designation & other contact links.
+
+- About - Contains short description and/or career goals.
+
+- Experience - Previous work experiences in chronological order.
+
+- Education - Education details in chronological order.
+
+- Projects - Projects undertaken with links to source/live version.
+
+- Skills - Lists acquired skills and industrial knowledge.
---
+++ gulpfile.js
@@ -0,0 +1,54 @@
+var gulp = require("gulp");
+var plumber = require("gulp-plumber");
+var uglify = require("gulp-uglify");
+var sass = require("gulp-sass")(require("sass"));
+var wait = require("gulp-wait");
+var rename = require("gulp-rename");
+var autoprefixer = require("gulp-autoprefixer");
+// var imagemin = require('gulp-imagemin');
+const { series } = require("gulp");
+
+gulp.task("scripts", function () {
+    return gulp
+        .src("scripts/scripts.js")
+        .pipe(
+            plumber(
+                plumber({
+                    errorHandler: function (err) {
+                        console.log(err);
+                        this.emit("end");
+                    },
+                })
+            )
+        )
+        .pipe(uglify({ output: { comments: "/^!/" } }))
+        .pipe(rename({ extname: ".min.js" }))
+        .pipe(gulp.dest("scripts"));
+});
+
+gulp.task("styles", function () {
+    return gulp
+        .src("styles/styles.scss")
+        .pipe(wait(250))
+        .pipe(
+            sass({
+                outputStyle: "compressed",
+            }).on("error", sass.logError)
+        )
+        .pipe(rename({ extname: ".css" }))
+        .pipe(gulp.dest("styles"));
+});
+
+// Optimize Images
+// gulp.task('images', () =>
+//     gulp.src('images/*')
+//     .pipe(imagemin())
+//     .pipe(gulp.dest('min'))
+// );
+
+gulp.task("watch", function () {
+    gulp.watch("scripts/scripts.js", gulp.series("scripts"));
+    gulp.watch("styles/styles.scss", gulp.series("styles"));
+});
+
+exports.default = series("styles", "scripts");
