dfb3542c37c6f04b12e85ea07afb745a8da4a4d4
Tejeshwar <tejeshwarreddy25@gmail.com>
2024-03-19 21:52:23 +0530

    Added a border and fixed resume link

--- scripts/projects.js
+++ scripts/projects.js
@@ -248,7 +248,7 @@ const links = [
     {
         name: 'Resume',
         description: 'What I have done so far',
-        url: 'https://github.com/epicman25/epicman25.github.io/blob/main/Tejeshwar_Reddy.pdf',
+        url: 'https://github.com/epicman25/epicman25.github.io/blob/main/TejeshwarReddy_Resume.pdf',
         icon: 'fa-brands fa-wpforms',
         color: '#008abd'
     },
